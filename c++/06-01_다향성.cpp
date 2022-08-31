a24ced820a39c47c718efa1fd0223d7d221cb061
xxx-1203 <s2119@e-mirim.hs.kr>
2022-08-31 09:59:06 +0900

    소멸자 앞에 반드시 virtual 키워드 사용

--- c++/06-01_다향성.cpp
+++ c++/06-01_다향성.cpp
@@ -5,8 +5,12 @@ using namespace std;
 
 class Animal {
 public:
+	Animal() { cout << "Animal add" << endl; }
+	virtual ~Animal() { cout << "Animal delete" << endl; }
+
 	void eat(void) { cout << "동물먹이" << endl; }
-	void roar(void) { cout << "동물짖어" << endl; }
+	//roar함수를 가상함수테이블에 등록(동적 바인딩)
+	virtual void roar(void) { cout << "동물짖어" << endl; }
 	void walk(void) { cout << "동물걸어" << endl; }
 
 private:
@@ -17,19 +21,20 @@ private:
 
 class Tiger : public Animal {
 public:
-	void roar(void) { cout << "어흥" << endl; }
+	Tiger() { cout << "Tigel add" << endl; }
+	virtual ~Tiger() { cout << "Tigel delete" << endl; }
+	void roar(void) override { cout << "어흥" << endl; }
 };
 
 class Dog : public Animal {
 public:
+	Dog() { cout << "Dog add" << endl; }
+	virtual ~Dog() { cout << "Dog delete" << endl; }
 	void roar(void) { cout << "멍멍" << endl; }
 };
 
 void main(void)
 {
-	Animal animal;
-	animal.roar();
-
-	Dog dog;
-	dog.roar();
+	Animal* animal = new Animal();
+	delete animal;
 }
