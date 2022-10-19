d09859e12928b050cdbb7936b6c9302522b72012
xxx-2203 <s2119@e-mirim.hs.kr>
2022-10-19 09:17:12 +0900

    enum 활성화

--- c++/09-01 enum.cpp
+++ c++/09-01 enum.cpp
@@ -3,30 +3,40 @@
 
 using namespace std;
 
+enum Food {
+	SESI,
+	UIKEI,
+	HAMBEGER,
+	COUNT
+};
 
 int main(void)
 {
-	string food_spot[3];
+	string food_spot[Food::COUNT];
 
-	food_spot[0] = "스시집";
-	food_spot[1] = "육회집";
-	food_spot[2] = "치킨집";
+	food_spot[Food::SESI] = "스시집";
+	food_spot[Food::UIKEI] = "육회집";
+	food_spot[Food::HAMBEGER] = "치킨집";
 
 	cout << "메뉴선택" << endl;
 	int menu = 1;
 
 	switch (menu)
 	{
-	case 0:
+	case Food::SESI:
 		cout << "스시 :";
 		break;
-	case 1:
+	case Food::UIKEI:
 		cout << "육회 :";
 		break;
-	case 2:
+	case Food::HAMBEGER:
 		cout << "치킨 :";
 		break;
 	default:
 		break;
 	}
+	cout << food_spot[menu] << endl;
+
+	return 0;
+
 }
